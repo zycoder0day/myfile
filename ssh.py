f2d0c6e95a165abf4d8584de5a746011a0917f9a
Zekkel AR <40993331+ZekkelAR@users.noreply.github.com>
2022-07-09 02:31:56 +0700

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -1,15 +1,22 @@
 import os
 import sys
 import time
+import subprocess
 
-print('[x] Installing screen')
-os.system('apt install screen')
-time.sleep(5)
-print('[x] downloading bot.py')
-os.system('curl https://pastebin.com/raw/JmzDQGvs -o bot.py')
-time.sleep(3)
-print('[x] Downloading requirements ')
-os.system('curl https://pastebin.com/raw/bNWEAw6w -o requirements.txt')
-print('[x] Installing pip')
-os.system('pip install -r requirements.txt')
-os.system('screen -R kontol')
+
+def test():
+	print('[x] Installing screen')
+	os.system('apt install screen')
+	time.sleep(5)
+	print('[x] downloading bot.py')
+	os.system('curl https://pastebin.com/raw/JmzDQGvs -o bot.py')
+	time.sleep(3)
+	print('[x] Downloading requirements ')
+	os.system('curl https://pastebin.com/raw/bNWEAw6w -o requirements.txt')
+	print('[x] Installing pip')
+	os.system('pip install -r requirements.txt')
+	p = subprocess.Popen("screen -R kontol", stdin=subprocess.PIPE, shell=True)
+	p.communicate(input='\n'.encode()) 
+	os.system('python3 bot.py')
+
+test()
